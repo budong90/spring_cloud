b43d7c611c2f83229c98241377e030ec3869f4a6
sunli <sunli@ywwl.com>
2018-08-16 11:56:00 +0800

    init

--- sc-f-chapter5/service-zuul/src/main/java/com/forezp/servicezuul/filter/MyFilter.java
+++ sc-f-chapter5/service-zuul/src/main/java/com/forezp/servicezuul/filter/MyFilter.java
@@ -2,22 +2,24 @@ package com.forezp.servicezuul.filter;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 /**
- * Email miles02@163.com
- *
- * @author fangzhipeng
- * create 2018-07-09
- **/
+ * @author sunli
+ * @create 2018-08-16 10:57
+ */
+
 @Component
 public class MyFilter extends ZuulFilter {
 
     private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
     @Override
     public String filterType() {
         return "pre";
@@ -34,19 +36,19 @@ public class MyFilter extends ZuulFilter {
     }
 
     @Override
-    public Object run() {
+    public Object run() throws ZuulException {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
         log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
         Object accessToken = request.getParameter("token");
-        if(accessToken == null) {
+        if (accessToken == null) {
             log.warn("token is empty");
             ctx.setSendZuulResponse(false);
             ctx.setResponseStatusCode(401);
             try {
                 ctx.getResponse().getWriter().write("token is empty");
-            }catch (Exception e){}
-
+            } catch (Exception e) {
+            }
             return null;
         }
         log.info("ok");
